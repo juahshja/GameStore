a752a0b0fbb319c98d73fafa5eb82975080990a8
geku1 <diegocaceda31@gmail.com>
2025-10-16 18:26:32 -0500

    Update app.routes.ts
    Signed-off-by: geku1 <diegocaceda31@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CarritoComponent } from './components/carrito/carrito';
+import { ProductosComponent } from './components/productos/productos';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'productos', pathMatch: 'full' },
+  { path: 'productos', component: ProductosComponent },
+  { path: 'carrito', component: CarritoComponent },
+  { path: '**', redirectTo: 'productos' }
+];
